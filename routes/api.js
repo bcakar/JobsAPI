8b569d3e9ba3c11568786282b3293850d5ae9eae
Berkay Cakar <berkaycakar@gmail.com>
2025-03-28 02:51:02 +0100

    swagger ui

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
-const router = require('');
-const yamljs = require('yamljs');
+const router = require('express').Router();
+const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
+const path = require('path');
 
-const swaggerDocument = yamljs.load('../swagger.yaml');
-router.get('/', swaggerUI.server, swaggerUI.setup(swaggerDocument));
+const swaggerFilePath = path.join(__dirname, '../swagger.yaml');
+
+const swaggerDocument = YAML.load(swaggerFilePath);
+router.get('/', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const v1AuthRoutes = require('./v1/auth');
 const v1JobsRoutes = require('./v1/jobs');
+const apiDocs = require('./api');
 
 const authMiddleware = require('../middleware/authentication');
 
+router.use('/', apiDocs);
 router.use('/api/v1/auth', v1AuthRoutes);
 router.use('/api/v1/jobs', authMiddleware, v1JobsRoutes);
 
